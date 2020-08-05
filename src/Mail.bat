50c360a2aae64e7bea5f67b2cea237bcf855063d
AttafHabib <attufhabib@gmail.com>
2020-08-05 21:31:42 +0500

    Inclcuded batch file

---
+++ src/Mail.bat
@@ -0,0 +1,2 @@
+javac --module-path "javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml Main.java
+java --module-path "javafx-sdk-11.0.2\lib" --add-modules javafx.controls,javafx.fxml Main.java
